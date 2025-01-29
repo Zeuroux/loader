3d4c1a69d06d672482d69c6b5827cc8d4ee15946
Zeuroux <zeyrox1091@gmail.com>
2025-01-29 22:46:55 +0800

    sd

--- run.bat
+++ run.bat
@@ -1,9 +1,35 @@
 @echo off
-ECHO Building jar
+echo Building jar
 call gradlew :minecraft:createFullJarRelease
-ECHO Sanitize jar
-unzip -q ./minecraft/build/intermediates/full_jar/release/createFullJarRelease/full.jar -d temp_jar && find temp_jar -type f -name "*.class" ! -name "NotificationListenerService.class" ! -name "Launcher.class" ! -name "GooglePlayStore.class" ! -name "AmazonAppStore.class" -delete && cd temp_jar && jar cf ../modified_full.jar * && cd .. && rm -rf temp_jar
-ECHO Converting to dex
-call d8 ".\minecraft\build\intermediates\full_jar\release\createFullJarRelease\full.jar"
-ECHO Copying
+echo Sanitize jar
+tar --extract --file=./minecraft/build/intermediates/full_jar/release/createFullJarRelease/full.jar --directory=temp_jar && find temp_jar -type f -name "*.class" ! -name "NotificationListenerService.class" ! -name "Launcher.class" ! -name "GooglePlayStore.class" ! -name "AmazonAppStore.class" -delete && tar --create --file=./modified_full.tar -C temp_jar . && rm -rf temp_jar
+REM Ensure temp_jar directory is created
+if not exist temp_jar (
+    mkdir temp_jar
+)
+echo Extract the JAR file 
+tar -xvf .\minecraft\build\intermediates\full_jar\release\createFullJarRelease\full.jar -C temp_jar
+REM Delete unwanted .class files, preserving specific files
+for /R temp_jar %%F in (*.class) do (
+    if /I not "%%~nxF"=="NotificationListenerService.class" (
+        if /I not "%%~nxF"=="Launcher.class" (
+            if /I not "%%~nxF"=="GooglePlayStore.class" (
+                if /I not "%%~nxF"=="AmazonAppStore.class" (
+                    del "%%F"
+                )
+            )
+        )
+    )
+)
+echo Recreate the JAR archive
+cd temp_jar
+jar -cfM ..\classes.jar *
+cd ..
+echo Clean up the temp_jar folder
+rmdir /S /Q temp_jar
+echo Converting to dex
+call d8 "classes.jar"
+echo Copying
 copy "./classes.dex" "./app/src/main/assets/launcher.dex"
+echo Cleanup
+del modified_full.jar classes.jar classes.dex
